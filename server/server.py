857143de7095ee1833cbb59e5d9362e7ec80957b
Abdu-Lateef Lekan-Fadeyi <tomiwafadeyi@gmail.com>
2025-03-07 19:18:48 -0500

    Updated CORS

--- server/server.py
+++ server/server.py
@@ -1,18 +1,23 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS
 import utils
 
 app = Flask(__name__)
+CORS(app, resources={r"/classify": {"origins": "https://political-figure-classifier.vercel.app"}})
 
 @app.route('/classify', methods=['GET', 'POST'])
 def classify():
-    img_b64 = request.form['image_data']
+    img_b64 = request.json['image_data']
 
     # Classify the image and return our results
     res = jsonify(utils.classify_image(img_b64))
-    res.headers.add('Access-Control-Allow-Origin', '*')
+
+    if not res:
+        return make_response(jsonify({'error': 'Image is unclear'}), 400)
+
     return res
 
 # Run server
 if __name__ == "__main__":
-    utils.load_artifacts() # Load our model and label dict
+    utils.load_artifacts()  # Load our model and label dict
     app.run(port=3000)
